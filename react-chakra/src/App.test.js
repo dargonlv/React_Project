ecd1b7f7a35decd068043cf064fa245007ae3c01
dargonlv <fatihmer0029@gmail.com>
2023-05-26 14:53:38 +0300

    test

--- react-chakra/src/App.test.js
+++ react-chakra/src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import {BrowserRouter} from "react-router-dom"
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('api kontrol', () => {
+    const {getByPlaceholderText,debug,container}= render(
+    <BrowserRouter>
+    <ChakraProvider>
+    <React.StrictMode>
+      <App></App>
+    </React.StrictMode>
+    </ChakraProvider>
+    </BrowserRouter>
+    )
+     
+    fireEvent.change(getByPlaceholderText("Search"),{target: {value:"car"}})
+    expect(getByPlaceholderText("Search")).toBeInTheDocument()
+    expect(container.querySelector(".sag")).not.toBeEmptyDOMElement()
+    expect(container.querySelector(".sag").firstChild).tohaveclass(".chakra-card")
+    console.log("//222/",container.querySelectorAll(".chakra-card").length)
 });

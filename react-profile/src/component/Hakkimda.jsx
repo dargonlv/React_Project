67758763746395f4bd5f773e9a1623458c624aed
dargonlv <fatihmer0029@gmail.com>
2023-06-05 16:09:14 +0300

    .

--- react-profile/src/component/Hakkimda.jsx
+++ react-profile/src/component/Hakkimda.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "../css/Hakkimda.css"
+import logo from "../clogo.png"
+
 
 function Hakkimda() {
   return (
@@ -8,10 +10,62 @@ function Hakkimda() {
         <div className='parca'> 
 
         </div>
-        <div style={{marginRight:20,marginBottom:6,fontSize:20,fontWeight:"bold"}}>
+        <div style={{alignself: "stretch",marginRight:20,marginLeft:80,marginTop:10,fontSize:20,fontWeight:"bold"}}>
 
           Hakkimda
         </div>
+      
+      </div>
+      <div className='ara'>
+
+      </div>
+
+      <div className='hakkindabody'>
+        <div className='icerik'>
+          <div className='icerikic'>
+            <div className='baslik'> 
+              Ben Bilişim Uzmanıyım
+            </div>
+            <div className='metin'>
+              Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral 
+              Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral 
+              Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral 
+              Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral 
+              Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral 
+              Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral 
+              Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral 
+              Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral 
+              Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral Ben Fatih Meral 
+            </div>
+          </div>
+
+        </div>
+        <div className='dilbaslik'>
+          <div>
+
+            Başlık
+          </div>
+        </div>
+
+        <div className='diller'>
+          <div className='dilleric'>
+            <div className='logo' >
+              <img src={logo}></img>
+            </div>
+              <div className='baslik'>C#</div>
+            <div className='metin'>
+              Bu alanda 4 yıllık bir tecrübem bulunmaktadır bir çok proje gelişiminde katkıda bulundum
+            </div>
+
+          </div>
+          <div className='dilleric'>
+          </div>
+          <div className='dilleric'>
+          </div>
+          <div className='dilleric'>
+          </div>
+        </div>
+
       </div>
 
     </div>
